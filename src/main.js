a008a2515e4229b4fd5d0a03e30a7e56071549c6
George Mihov <gmihov001@gmail.com>
2020-12-04 12:55:18 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -6,5 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our", "your"];
+  var adj = ["great", "big", "amazing"];
+  var noun = ["jogger", "racoon", "bloop"];
+  var extension = [".com", ".net", ".us", ".io"];
+
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var k = 0; k < noun.length; k++) {
+        for (var l = 0; l < noun.length; l++) {
+          document.querySelector("#domain_name").innerHTML +=
+            "<li> www." +
+            pronoun[i] +
+            adj[j] +
+            noun[k] +
+            extension[l] +
+            " </li>";
+        }
+      }
+    }
+  }
 };
